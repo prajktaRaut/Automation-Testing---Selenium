9eeed5796d881bff5564c6ecbda25dc535d5d9c4
singhprince0088 <singhprince0088@gmail.com>
2020-01-09 19:33:25 +0530

    Added test case to capture screenshots in Selenium using getScreenshotAs() method of TakesScreenshot interface

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,5 +1,12 @@
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 public class SeleniumTest extends BaseTest {
 
@@ -16,6 +23,26 @@ public class SeleniumTest extends BaseTest {
             System.out.println("pagesource of page is "+pageSource);
             Thread.sleep(2000);
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.close();
+    }
+
+    @Test
+    public void captureScreenshots() {
+        try {
+            Date d = new Date();
+            String date = d.toString();
+            System.out.println("Current date is "+date);
+            String newDate = date.replaceAll(":", "-");
+            System.out.println("New date is "+newDate);
+            System.setProperty("webdriver.chrome.driver","chromedriver");
+            driver.get("https://www.youtube.com/watch?v=T_4ZFj_KTx4");
+            TakesScreenshot takesScreenshot= (TakesScreenshot) driver;
+            File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            File destFile = new File("/home/admin1/Documents/PrajktaRaut/SeleniumDemo/src/test/resources/ScreenShots/image.jpg");
+            FileUtils.copyFile(srcFile,destFile);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         driver.close();
